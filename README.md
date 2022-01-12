21b2a9080c2a2a5a099553f4ffeae72c200a8519
Borys Chajdas <b.chajdas@intive.com>
2022-01-12 14:22:14 +0100

    Init project

---
+++ README.md
@@ -0,0 +1,1 @@
+# patronage22-android-szczecin
---
+++ app/src/main/java/com/example/patronage22_android_szczecin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.patronage22_android_szczecin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
