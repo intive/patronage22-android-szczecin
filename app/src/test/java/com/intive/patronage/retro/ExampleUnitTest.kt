7e8bbd88c0017bd99dd8c10f5ed4f4063f4a3f7b
catchanus <55312282+catchanus@users.noreply.github.com>
2022-01-16 18:12:54 +0100

    P2022-46-integrate-ktlint -> P2022-101-integrate-ktlint-plugin-wrapper

--- app/src/test/java/com/intive/patronage/retro/ExampleUnitTest.kt
+++ app/src/test/java/com/intive/patronage/retro/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.intive.patronage.retro
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
