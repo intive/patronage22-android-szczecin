9ad57395db27119bf61e045a8245606d2ca5f09a
Konrad <kmaslowiec@gmail.com>
2022-01-22 10:42:43 +0100

    P2022-278 navigation component -> P2022-279 add navigation

---
+++ app/src/main/java/com/intive/patronage/retro/main_fragment/MainFragment.kt
@@ -0,0 +1,19 @@
+package com.intive.patronage.retro.main_fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.intive.patronage.retro.R
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+}
