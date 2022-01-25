c11e1f404462dbd9e804726e5ad8249945e9b03e
Nataniel-Antosik <57491794+nataniel-antosik@users.noreply.github.com>
2022-01-25 11:30:41 +0100

    P2022-238 main screen bottom bar -> P2022-575 Add view binding

--- app/src/main/java/com/intive/patronage/retro/MainActivity.kt
+++ app/src/main/java/com/intive/patronage/retro/MainActivity.kt
@@ -3,14 +3,17 @@ package com.intive.patronage.retro
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.intive.patronage.retro.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModel()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Log.i("VIEWMODEL", "onCreate: ${viewModel.sayHello()}")
     }
 }
