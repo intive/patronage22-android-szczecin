12e28efb42855d3cf2afbea7761911868eb947b8
Nataniel-Antosik <57491794+nataniel-antosik@users.noreply.github.com>
2022-01-25 11:30:41 +0100

    P2022-237 Splash Screen -> P2022-539 Add splash screen

--- app/src/main/java/com/intive/patronage/retro/MainActivity.kt
+++ app/src/main/java/com/intive/patronage/retro/MainActivity.kt
@@ -3,6 +3,7 @@ package com.intive.patronage.retro
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.intive.patronage.retro.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -11,8 +12,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        splashScreen.setKeepOnScreenCondition { false }
         setContentView(binding.root)
         Log.i("VIEWMODEL", "onCreate: ${viewModel.sayHello()}")
     }
