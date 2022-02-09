3a424209adb423aa061460fc88db2ea8191d9ad1
catchanus <55312282+catchanus@users.noreply.github.com>
2022-02-09 08:38:44 +0100

    P2022-424 User logout -> P2022-992 Add logout in overflow menu

--- app/src/main/java/com/intive/patronage/retro/board_fragment/BoardsFragment.kt
+++ app/src/main/java/com/intive/patronage/retro/board_fragment/BoardsFragment.kt
@@ -2,8 +2,12 @@ package com.intive.patronage.retro.board_fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.intive.patronage.retro.MainActivity
+import com.intive.patronage.retro.R
 import com.intive.patronage.retro.databinding.BoardFragmentBinding
 
 class BoardsFragment : Fragment() {
@@ -12,4 +16,10 @@ class BoardsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ) = BoardFragmentBinding.inflate(inflater, container, false).root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val bottomAppBar = (activity as MainActivity).findViewById<BottomAppBar>(R.id.bottomAppBar)
+        bottomAppBar.replaceMenu(R.menu.bottom_app_bar_menu_boards)
+    }
 }
--- app/src/main/java/com/intive/patronage/retro/profile_fragment/ProfileFragment.kt
+++ app/src/main/java/com/intive/patronage/retro/profile_fragment/ProfileFragment.kt
@@ -5,29 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.intive.patronage.retro.MainActivity
+import com.intive.patronage.retro.R
 import com.intive.patronage.retro.databinding.ProfileFragmentBinding
 import com.intive.patronage.retro.firebase.FirebaseViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ProfileFragment : Fragment() {
     private val firebaseViewModel: FirebaseViewModel by viewModel()
-    private lateinit var binding: ProfileFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View {
-        binding = ProfileFragmentBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+    ) = ProfileFragmentBinding.inflate(inflater, container, false).root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnLogOut.setOnClickListener {
-            (activity as MainActivity).signIn()
-            firebaseViewModel.logOut()
+        val bottomAppBar = (activity as MainActivity).findViewById<BottomAppBar>(R.id.bottomAppBar)
+        bottomAppBar.replaceMenu(R.menu.bottom_app_bar_menu_profile)
+
+        bottomAppBar.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.menu_log_out -> {
+                    (activity as MainActivity).signIn()
+                    firebaseViewModel.logOut()
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
