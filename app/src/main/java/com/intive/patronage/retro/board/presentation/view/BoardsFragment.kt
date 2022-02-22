ada20729922a711fe30f3e11bccd59cf527c449e
Nataniel-Antosik <57491794+Nataniel-Antosik@users.noreply.github.com>
2022-02-22 18:01:08 +0100

    P2022-425 Boards screen -> P2022-1255 Modify bottom app bar

--- app/src/main/java/com/intive/patronage/retro/board/presentation/view/BoardsFragment.kt
+++ app/src/main/java/com/intive/patronage/retro/board/presentation/view/BoardsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.intive.patronage.retro.R
 import com.intive.patronage.retro.databinding.BoardFragmentBinding
 import com.intive.patronage.retro.main.presentation.view.MainActivity
@@ -20,6 +21,9 @@ class BoardsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val bottomAppBar = (activity as MainActivity).findViewById<BottomAppBar>(R.id.bottomAppBar)
+        val fab = (activity as MainActivity).findViewById<FloatingActionButton>(R.id.floatingButton)
+
         bottomAppBar.replaceMenu(R.menu.bottom_app_bar_menu_boards)
+        fab.show()
     }
 }
