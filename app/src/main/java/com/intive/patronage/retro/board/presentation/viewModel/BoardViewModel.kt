a22bf330d78833e2d5d7b2ef856e2a384df4d8cd
catchanus <55312282+catchanus@users.noreply.github.com>
2022-03-19 17:06:03 +0100

    P2022-1385 Create retro board -> P2022-1808 Add boards list refreshing

--- app/src/main/java/com/intive/patronage/retro/board/presentation/viewModel/BoardViewModel.kt
+++ app/src/main/java/com/intive/patronage/retro/board/presentation/viewModel/BoardViewModel.kt
@@ -1,6 +1,5 @@
 package com.intive.patronage.retro.board.presentation.viewModel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.intive.patronage.retro.board.model.repo.BoardRepository
@@ -10,7 +9,7 @@ import kotlinx.coroutines.Dispatchers
 
 class BoardViewModel(private val repo: BoardRepository) : ViewModel() {
 
-    val boards: LiveData<Resource<List<Board>>> = liveData(Dispatchers.IO) {
+    fun getBoards() = liveData(Dispatchers.IO) {
         emit(Resource.loading(null))
         emit(repo.getBoards())
     }
